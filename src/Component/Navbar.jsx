86748d388288685365f84a19de878de8c59deec0
CodeSShuv <codeshuv@gmail.com>
2024-11-24 09:36:35 +0545

    for image link

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <nav className="navbar">
   
         <div className="logo">
-            <img src="/EA/RBCG.png" alt="" />
+            <img src="./RBCG.png" alt="" />
         </div>
   
         <a target="_blank" className="join-now-btn" href="https://docs.google.com/forms/d/e/1FAIpQLSd7aO4PKA4m7QJ7fRwUdbSccyyrTpr3G4Y79NHZ9MrMHBfiVw/viewform?usp=sf_link"> Join Now</a>
